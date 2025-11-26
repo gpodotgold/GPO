c52014e94c36f49ccdab74cc4a849844debf8d4d
gpodotgold <211713959+gpodotgold@users.noreply.github.com>
2025-11-26 06:46:00 +0000

    feat: sdk layout

---
+++ sdk/src/index.ts
@@ -0,0 +1,24 @@
+import { PublicKey, Connection } from '@solana/web3.js';
+import { BN } from '@coral-xyz/anchor';
+
+export class GoldRootSDK {
+    programId: PublicKey;
+    connection: Connection;
+
+    constructor(connection: Connection, programId: string) {
+        this.connection = connection;
+        this.programId = new PublicKey(programId);
+    }
+
+    async recycleDust(user: PublicKey, amount: number): Promise<string> {
+        console.log(`Preparing to recycle ${amount} dust...`);
+        return "tx_hash_placeholder";
+    }
+
+    async getStats(): Promise<object> {
+        return {
+            totalRecycled: new BN(1000000),
+            gpoPrice: 1.25
+        };
+    }
+}
